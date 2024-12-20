bd61ec182c38a5ef66cb7dde4c63a42e097e1381
James Thigpen <james.r.thigpen@gmail.com>
2024-12-20 11:43:31 -0700

    access logs from express

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import dotenv from 'dotenv'
-import FeedGenerator from './server'
+import dotenv from 'dotenv';
+import FeedGenerator from './server';
 
 const run = async () => {
-  dotenv.config()
-  const hostname = maybeStr(process.env.FEEDGEN_HOSTNAME) ?? 'example.com'
+  dotenv.config();
+  const hostname = maybeStr(process.env.FEEDGEN_HOSTNAME) ?? 'example.com';
   const serviceDid =
-    maybeStr(process.env.FEEDGEN_SERVICE_DID) ?? `did:web:${hostname}`
+    maybeStr(process.env.FEEDGEN_SERVICE_DID) ?? `did:web:${hostname}`;
   const server = FeedGenerator.create({
     port: maybeInt(process.env.FEEDGEN_PORT) ?? 3000,
     listenhost: maybeStr(process.env.FEEDGEN_LISTENHOST) ?? 'localhost',
@@ -19,23 +19,26 @@ const run = async () => {
       maybeInt(process.env.FEEDGEN_SUBSCRIPTION_RECONNECT_DELAY) ?? 3000,
     hostname,
     serviceDid,
-  })
-  await server.start()
+  });
+  server.app.get('/', (req, res) => {
+    res.send('Feed Me, Seymour!');
+  });
+  await server.start();
   console.log(
     `🤖 running feed generator at http://${server.cfg.listenhost}:${server.cfg.port}`,
-  )
-}
+  );
+};
 
 const maybeStr = (val?: string) => {
-  if (!val) return undefined
-  return val
-}
+  if (!val) return undefined;
+  return val;
+};
 
 const maybeInt = (val?: string) => {
-  if (!val) return undefined
-  const int = parseInt(val, 10)
-  if (isNaN(int)) return undefined
-  return int
-}
+  if (!val) return undefined;
+  const int = parseInt(val, 10);
+  if (isNaN(int)) return undefined;
+  return int;
+};
 
-run()
+run();
