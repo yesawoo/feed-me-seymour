2f27061191c8da54e85afd7c1a5a3d9251dd6473
James Thigpen <james.r.thigpen@gmail.com>
2024-12-21 21:25:34 -0700

    Enrichment worker!

--- src/workers/enrichmentWorker.ts
+++ src/workers/enrichmentWorker.ts
@@ -5,6 +5,7 @@ import { LengthFilter } from '../events/filters/length'
 import { EventFilter, EventFilterHandler } from '../events/filters/filter'
 import { Config } from '../config'
 import Sentiment from 'sentiment'
+import hashtagRegex from 'hashtag-regex'
 
 export async function runEnrichmentWorker(config: Config) {
   const sourceUri = config.zmqUri['filteredEvents']
@@ -23,17 +24,25 @@ export async function runEnrichmentWorker(config: Config) {
     const event = JSON.parse(msg.toString()) as Event
     const enrichedEvent = enrich(event)
 
-    console.log(enrichedEvent, enrichedEvent)
+    console.log('Enriched event', JSON.stringify(enrichedEvent))
     // await sink.send(JSON.stringify(event))
   }
 }
 
 function enrich(event: Event): Event {
-  const newEvent = structuredClone(event)
+  let newEvent = structuredClone(event)
   if (!newEvent.labels) {
     newEvent.labels = []
   }
 
+  newEvent = analyzeSentiment(newEvent)
+  newEvent = extractHashtags(newEvent)
+
+  newEvent.labels.push({ key: 'enriched', value: true })
+  return newEvent
+}
+
+function analyzeSentiment(newEvent: Event): Event {
   const sentiment = new Sentiment()
   const result = sentiment.analyze(newEvent.data.record.text)
   newEvent.labels.push({ key: 'sentimentScore', value: result.score })
@@ -41,6 +50,18 @@ function enrich(event: Event): Event {
     key: 'sentimentComparative',
     value: result.comparative,
   })
-  newEvent.labels.push({ key: 'enriched', value: true })
+  return newEvent
+}
+function extractHashtags(newEvent: Event): Event {
+  const regex = hashtagRegex()
+  let match
+  const hashtags = new Set<string>()
+  while ((match = regex.exec(newEvent.data.record.text))) {
+    const hashtag = match[0]
+    hashtags.add(hashtag)
+  }
+  for (const hashtag of hashtags) {
+    newEvent.labels.push({ key: 'hashtag', value: hashtag })
+  }
   return newEvent
 }
